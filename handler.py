2b1cc049f94aee5cfe7e89b1da3b7f0121c7269e
Lasith Manujitha <Manujitha98@gmail.com>
2024-03-04 01:58:32 +0530

    :sparkles: Update handler.py to include additional methods

--- handler.py
+++ handler.py
@@ -12,30 +12,58 @@ from seedr import SeedrAPI
 
 class Handler:
     def __init__(self):
-        self.seedr = SeedrAPI(email="zincorecoc@gmail.com", password="Lasith@lasith@1998")
+        self.seedr = SeedrAPI(
+            email="zincorecoc@gmail.com", password="Lasith@lasith@1998"
+        )
         self.account = self.seedr.get_drive()
-        
+
     def get_drive(self):
-        space_max = self.account['space_max']
-        space_used = self.account['space_used']
-        
+        space_max = self.account["space_max"]
+        space_used = self.account["space_used"]
+
         return space_max, space_used
-    
+
     def get_folders(self):
-        folders = self.account['folders']
+        folders = self.account["folders"]
         return folders
-    
+
     def get_files(self):
-        files = self.account['files']
+        files = self.account["files"]
         return files
-    
+
     def get_all(self):
         folders = self.get_folders()
         files = self.get_files()
         all = folders + files
-        
+
         return all
-        
-        
+
+    def get_folder(self, folder_id):
+        folder = self.seedr.get_folder(folder_id)
+
+        for folder in all:
+            if "id" in folder and folder["id"] == folder_id:
+                return folder["name"], folder["size"]
+
+        return None
+
+    def get_file(self, folder_file_id):
+        all = self.get_all()
+
+        for file in all:
+            if "folder_file_id" in file and file["folder_file_id"] == folder_file_id:
+                return file["name"], file["size"]
+
+        return None
+
+    def add_torrent(self, torrent_url):
+        return self.seedr.add_torrent(torrent_url)
+
+    def del_folder(self, folder_id):
+        self.seedr.delete_folder(folder_id)
+
+    def del_file(self, folder_file_id):
+        self.seedr.delete_file(folder_file_id)
+
+
 handler = Handler()
-print(handler.get_all())
